ea8e27b0c513d2779749170d1e753ce6f428082c
Enrique Querini <ptyelnegro@gmail.com>
2022-08-22 01:57:38 -0500

    añadiendo todos los documentos para el challenge3

---
+++ Display.js
@@ -0,0 +1,9 @@
+class Display {
+    constructor(numbercard,name,month,cvc) {
+        this.cardnumber = numbercard;
+        this.cardholder = name;
+        this.month = month;
+        this.cvc = cvc;
+
+    }
+}
