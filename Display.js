bf72d3ad0da5d55300130051bfbb1bd5fac4befc
Enrique Querini <ptyelnegro@gmail.com>
2022-08-23 12:33:44 -0500

    Limpiando(eliminando) todo mi branch reto4

--- Display.js
+++
@@ -1,9 +0,0 @@
-class Display {
-    constructor(numbercard,name,month,cvc) {
-        this.cardnumber = numbercard;
-        this.cardholder = name;
-        this.month = month;
-        this.cvc = cvc;
-
-    }
-}
