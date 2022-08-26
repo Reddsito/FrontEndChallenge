c2d15d14db985f3e79bbd9bad9dab474dd23d61a
Enrique Querini <ptyelnegro@gmail.com>
2022-08-25 22:17:40 -0500

    Introduciendo todos los archivos del reto 4

---
+++ script.js
@@ -0,0 +1,112 @@
+async function getDays() {
+    const resPost = await fetch('data.json');
+    const post = await resPost.json();
+    const total = document.querySelector('#totalAmount')
+    let initialValue = 0;
+    const bg = createBg(post);
+    crearChart(post,bg);
+
+
+    
+}
+function crearChart(labels,bgColorAndHover) {
+    const ctx = document.getElementById('Chart');
+      new Chart(ctx, {
+        type: 'bar',
+        data: {
+            datasets: [{
+                data: labels,
+                backgroundColor: bgColorAndHover[0],
+                hoverBackgroundColor: bgColorAndHover[1],
+                borderWidth: 0,
+                borderRadius: 7
+            }]
+        },
+        options: {
+            scales: {
+                x: {
+                  grid: {
+                    display: false,
+                    drawBorder: false
+                  },
+                  ticks: {
+                    color: "#93867B",
+                  }
+                },
+                y: {
+                  grid: {
+                    display: false,
+                    borderColor: 'transparent'
+                  },
+                  ticks: {
+                    display: false
+                  }
+                }
+              },
+              plugins: {
+                legend: {
+                  display: false
+                },
+                tooltip: {
+                  callbacks: {
+                      label: function(context) {
+                          let label = context.dataset.label || '';
+  
+                          if (label) {
+                              label += ': ';
+                          }
+                          if (context.parsed.y !== null) {
+                              label += new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(context.parsed.y);
+                          }
+                          return label;
+                      },
+    
+                      
+                  }
+              }
+              },
+              parsing: {
+                xAxisKey: 'day',
+                yAxisKey: 'amount'
+            },
+        }
+    });
+}
+
+function createBg(post) {
+
+  let maxValue = 0;
+  let aux = 0;
+  let backGroundArr = [];
+  let backGroundHoverArr = [];
+
+  for (let i = 0; i < post.length; i++) {
+    if(post[i].amount > aux) {
+      aux = post[i].amount;
+      maxValue = i;
+    }
+}
+
+for (let i = 0; i < post.length; i++){
+  if(i == maxValue) {
+    backGroundArr.push('rgb(118, 181, 188)');
+    backGroundHoverArr.push('rgb(118, 181, 188, .5)');
+  } else {
+    backGroundArr.push('rgb(236, 119, 95)');
+    backGroundHoverArr.push('rgb(236, 119, 95, .55)');
+  }
+
+
+
+}
+
+return [backGroundArr,backGroundHoverArr ];
+}
+getDays();
+
+
+
+
+
+
+
