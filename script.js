88373a3f0b054425d4250e3bf16c2f1e0c6ca1f7
Enrique Querini <ptyelnegro@gmail.com>
2022-08-27 18:46:07 -0500

    Subiendo archivos del reto5

--- script.js
+++ script.js
@@ -1,100 +1,29 @@
-async function getDays() {
-    const resPost = await fetch('data.json');
-    const post = await resPost.json();
-    const total = document.querySelector('#totalAmount')
-    let initialValue = 0;
-    const bg = createBg(post);
-    crearChart(post,bg);
+const inputs = document.querySelectorAll('.inputs');
+const text = document.querySelector('#text');
+const form = document.querySelector('#form');
+const box = document.querySelector('#box');
 
-    
-}
-function crearChart(labels,bgColorAndHover) {
-    const ctx = document.getElementById('Chart');
-      new Chart(ctx, {
-        type: 'bar',
-        data: {
-            datasets: [{
-                data: labels,
-                backgroundColor: bgColorAndHover[0],
-                hoverBackgroundColor: bgColorAndHover[1],
-                borderWidth: 0,
-                borderRadius: 7
-            }]
-        },
-        options: {
-            scales: {
-                x: {
-                  grid: {
-                    display: false,
-                    drawBorder: false
-                  },
-                  ticks: {
-                    color: "#93867B",
-                  }
-                },
-                y: {
-                  grid: {
-                    display: false,
-                    borderColor: 'transparent'
-                  },
-                  ticks: {
-                    display: false
-                  }
-                }
-              },
-              plugins: {
-                legend: {
-                  display: false
-                },
-                tooltip: {
-                  callbacks: {
-                      label: function(context) {
-                          let label = context.dataset.label || '';
-  
-                          if (label) {
-                              label += ': ';
-                          }
-                          if (context.parsed.y !== null) {
-                              label += new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(context.parsed.y);
-                          }
-                          return label;
-                      },
-    
-                      
-                  }
-              }
-              },
-              parsing: {
-                xAxisKey: 'day',
-                yAxisKey: 'amount'
-            },
-        }
-    });
-}
+inputs.forEach(input => {
+    input.addEventListener("click",  () => {
+        inputs.forEach(input2 => {
+            if(input2.checked) {
+               input2.parentElement.classList.add("--active")
+               text.textContent = `You selected ${input2.previousElementSibling.textContent}  out of 5`;
+            } else {
+                input2.parentElement.classList.remove("--active")
+            }
+        })
+            
+    })
+});
 
-function createBg(post) {
-
-  let backGroundArr = [];
-  let backGroundHoverArr = [];
-  const today = new Date();
-  const dayActual = new Date(today).getDay();
-
-for (let i = 0; i < post.length; i++){
-  if(i == dayActual-1) {
-    backGroundArr.push('rgb(118, 181, 188)');
-    backGroundHoverArr.push('rgb(118, 181, 188, .5)');
-  } else {
-    backGroundArr.push('rgb(236, 119, 95)');
-    backGroundHoverArr.push('rgb(236, 119, 95, .55)');
-  }
-}
-return [backGroundArr,backGroundHoverArr ];
-}
-getDays();
-
-
-
-
-
-
+form.addEventListener('submit', (e)=> {
+    e.preventDefault();
+    if(text.textContent == ""){
+        alert("Select an option before sending...")
+    } else {
+        box.classList.add("box--hide");
+    }
+        
+})
 
--- style-guide.md
+++ style-guide.md
@@ -11,23 +11,23 @@ The designs were created to the following widths:
 
 ### Primary
 
-- Soft red: hsl(10, 79%, 65%)
-- Cyan: hsl(186, 34%, 60%)
+- Orange: hsl(25, 97%, 53%)
 
 ### Neutral
 
-- Dark brown: hsl(25, 47%, 15%)
-- Medium brown: hsl(28, 10%, 53%)
-- Cream: hsl(27, 66%, 92%)
-- Very pale orange: hsl(33, 100%, 98%)
+- White: hsl(0, 0%, 100%)
+- Light Grey: hsl(217, 12%, 63%)
+- Medium Grey: hsl(216, 12%, 54%)
+- Dark Blue: hsl(213, 19%, 18%)
+- Very Dark Blue: hsl(216, 12%, 8%)
 
 ## Typography
 
 ### Body Copy
 
-- Font size: 18px
+- Font size (paragraph): 15px
 
 ### Font
 
-- Family: [DM Sans](https://fonts.google.com/specimen/DM+Sans)
+- Family: [Overpass](https://fonts.google.com/specimen/Overpass)
 - Weights: 400, 700
