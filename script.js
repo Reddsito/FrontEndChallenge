78a8f7756b8a50bfa91cd461f3833de4d56634b1
Enrique Querini <ptyelnegro@gmail.com>
2022-08-29 01:13:22 -0500

    Subiendo archivos del reto6

--- script.js
+++ script.js
@@ -1,29 +1,45 @@
-const inputs = document.querySelectorAll('.inputs');
-const text = document.querySelector('#text');
-const form = document.querySelector('#form');
-const box = document.querySelector('#box');
+const bar = document.querySelector('.nav__bar');
+const equis = document.querySelector('.menu_equis');
+const menu = document.querySelector('.nav_menu');
+const nav = document.querySelector('.nav');
+const links = document.querySelectorAll('.menu-link');
+const main = document.querySelector('.main');
+const mediaQueryList = window.matchMedia("screen and (max-width: 900px)");
+const mainBoxImg = document.querySelector('.main__box-image');
 
-inputs.forEach(input => {
-    input.addEventListener("click",  () => {
-        inputs.forEach(input2 => {
-            if(input2.checked) {
-               input2.parentElement.classList.add("--active")
-               text.textContent = `You selected ${input2.previousElementSibling.textContent}  out of 5`;
-            } else {
-                input2.parentElement.classList.remove("--active")
-            }
-        })
-            
-    })
+if(mediaQueryList.matches) {
+    mainBoxImg.src="images/image-hero-mobile.png";
+} else {
+    mainBoxImg.src="images/image-hero-desktop.png";
+}
+bar.addEventListener("click", () => {
+    nav.classList.add('--active-mobile');
+    main.classList.add('--active-bg');
 });
 
-form.addEventListener('submit', (e)=> {
-    e.preventDefault();
-    if(text.textContent == ""){
-        alert("Select an option before sending...")
-    } else {
-        box.classList.add("box--hide");
-    }
-        
-})
+equis.addEventListener("click", () => {
+    nav.classList.remove('--active-mobile');
+    main.classList.remove('--active-bg');
+});
 
+window.onresize = () => {
+    nav.classList.remove('--active-mobile');
+    links.forEach(link => {
+        link.parentElement.parentElement.classList.remove('--active-link');
+    })
+    nav.classList.remove('--active-mobile');
+    main.classList.remove('--active-bg');
+
+    if(mediaQueryList.matches) {
+        mainBoxImg.src="images/image-hero-mobile.png";
+    } else {
+        mainBoxImg.src="images/image-hero-desktop.png";
+    }
+
+};
+
+links.forEach(link => {
+    link.addEventListener("click", (e) => {
+        link.parentElement.parentElement.classList.toggle('--active-link');
+    })
+})
